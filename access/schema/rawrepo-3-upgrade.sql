fe98696c1e94ab814fe16aa7e2526fe3dd3db810
Morten Bøgeskov <mb@dbc.dk>
2014-10-02 11:14:36 +0000

    relation foreign key to "not deleted" records
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@77178 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/schema/rawrepo-3-upgrade.sql
+++ access/schema/rawrepo-3-upgrade.sql
@@ -20,3 +20,79 @@ BEGIN
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
+
+
+ALTER TABLE relations DROP CONSTRAINT relations_fk_owner;
+
+ALTER TABLE relations DROP CONSTRAINT relations_fk_refer;
+
+
+
+CREATE OR REPLACE FUNCTION validate_relation_fk() RETURNS TRIGGER AS $$ -- V3
+DECLARE
+    deletedRecord BOOLEAN;
+BEGIN
+    SELECT COUNT(*) = CAST(1 AS BIGINT) INTO deletedRecord FROM records WHERE bibliographicrecordid=NEW.bibliographicrecordid AND agencyid=NEW.agencyid AND deleted=TRUE;
+    IF deletedRecord THEN
+        RAISE EXCEPTION 'RELATION FROM DELETED';
+    END IF;
+    SELECT COUNT(*) = CAST(1 AS BIGINT) INTO deletedRecord FROM records WHERE bibliographicrecordid=NEW.refer_bibliographicrecordid AND agencyid=NEW.refer_agencyid AND deleted=TRUE;
+    IF deletedRecord THEN
+        RAISE EXCEPTION 'RELATION TO DELETED';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_relation_fk_insert -- V3
+    AFTER INSERT ON relations
+    FOR EACH ROW
+    EXECUTE PROCEDURE validate_relation_fk();
+
+CREATE TRIGGER validate_relation_fk_update -- V3
+    AFTER UPDATE ON relations
+    FOR EACH ROW
+    WHEN (OLD.* IS DISTINCT FROM NEW.*)
+    EXECUTE PROCEDURE validate_relation_fk();
+
+
+CREATE OR REPLACE FUNCTION validate_relation_fk_rev() RETURNS TRIGGER AS $$ -- V3
+DECLARE
+    hasRelations BOOLEAN;
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=OLD.bibliographicrecordid AND agencyid=OLD.agencyid OR
+                                                                   refer_bibliographicrecordid=OLD.bibliographicrecordid AND refer_agencyid=OLD.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO DELETED (PURGED)';
+        END IF;
+    END IF;
+    IF (TG_OP = 'UPDATE' AND (OLD.bibliographicrecordid <> NEW.bibliographicrecordid OR OLD.agencyid <> NEW.agencyid)) THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=OLD.bibliographicrecordid AND agencyid=OLD.agencyid OR
+                                                                   refer_bibliographicrecordid=OLD.bibliographicrecordid AND refer_agencyid=OLD.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO OLD ID (ID CHANGED)';
+        END IF;
+    END IF;
+    IF (TG_OP = 'UPDATE' AND NEW.deleted) THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=NEW.bibliographicrecordid AND agencyid=NEW.agencyid OR
+                                                                   refer_bibliographicrecordid=NEW.bibliographicrecordid AND refer_agencyid=NEW.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO DELETED';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_relation_fk_rev_update -- V3
+    AFTER UPDATE ON records
+    FOR EACH ROW
+    WHEN (OLD.* IS DISTINCT FROM NEW.*)
+    EXECUTE PROCEDURE validate_relation_fk_rev();
+
+CREATE TRIGGER validate_relation_fk_rev_delete -- V3
+    AFTER DELETE ON records
+    FOR EACH ROW
+    EXECUTE PROCEDURE validate_relation_fk_rev();
+
--- access/schema/rawrepo-3.sql
+++ access/schema/rawrepo-3.sql
@@ -73,10 +73,85 @@ CREATE TABLE relations ( -- V2
        agencyid NUMERIC(6) NOT NULL,
        refer_bibliographicrecordid VARCHAR(64) NOT NULL,
        refer_agencyid NUMERIC(6) NOT NULL,
-       CONSTRAINT relations_pk PRIMARY KEY (bibliographicrecordid, agencyid, refer_bibliographicrecordid, refer_agencyid),
-       CONSTRAINT relations_fk_owner FOREIGN KEY (bibliographicrecordid, agencyid) REFERENCES records(bibliographicrecordid, agencyid),
-       CONSTRAINT relations_fk_refer FOREIGN KEY (refer_bibliographicrecordid, refer_agencyid) REFERENCES records(bibliographicrecordid, agencyid)
+       CONSTRAINT relations_pk PRIMARY KEY (bibliographicrecordid, agencyid, refer_bibliographicrecordid, refer_agencyid)
+--V3       CONSTRAINT relations_fk_owner FOREIGN KEY (bibliographicrecordid, agencyid) REFERENCES records(bibliographicrecordid, agencyid),
+--V3       CONSTRAINT relations_fk_refer FOREIGN KEY (refer_bibliographicrecordid, refer_agencyid) REFERENCES records(bibliographicrecordid, agencyid)
 );
+
+--
+-- Validate relation to not deleted record
+--
+CREATE OR REPLACE FUNCTION validate_relation_fk() RETURNS TRIGGER AS $$ -- V3
+DECLARE
+    deletedRecord BOOLEAN;
+BEGIN
+    SELECT COUNT(*) = CAST(1 AS BIGINT) INTO deletedRecord FROM records WHERE bibliographicrecordid=NEW.bibliographicrecordid AND agencyid=NEW.agencyid AND deleted=TRUE;
+    IF deletedRecord THEN
+        RAISE EXCEPTION 'RELATION FROM DELETED';
+    END IF;
+    SELECT COUNT(*) = CAST(1 AS BIGINT) INTO deletedRecord FROM records WHERE bibliographicrecordid=NEW.refer_bibliographicrecordid AND agencyid=NEW.refer_agencyid AND deleted=TRUE;
+    IF deletedRecord THEN
+        RAISE EXCEPTION 'RELATION TO DELETED';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_relation_fk_insert -- V3
+    AFTER INSERT ON relations
+    FOR EACH ROW
+    EXECUTE PROCEDURE validate_relation_fk();
+
+CREATE TRIGGER validate_relation_fk_update -- V3
+    AFTER UPDATE ON relations
+    FOR EACH ROW
+    WHEN (OLD.* IS DISTINCT FROM NEW.*)
+    EXECUTE PROCEDURE validate_relation_fk();
+
+
+--
+-- Validate relation to newly deleted record
+--
+CREATE OR REPLACE FUNCTION validate_relation_fk_rev() RETURNS TRIGGER AS $$ -- V3
+DECLARE
+    hasRelations BOOLEAN;
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=OLD.bibliographicrecordid AND agencyid=OLD.agencyid OR
+                                                                   refer_bibliographicrecordid=OLD.bibliographicrecordid AND refer_agencyid=OLD.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO DELETED (PURGED)';
+        END IF;
+    END IF;
+    IF (TG_OP = 'UPDATE' AND (OLD.bibliographicrecordid <> NEW.bibliographicrecordid OR OLD.agencyid <> NEW.agencyid)) THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=OLD.bibliographicrecordid AND agencyid=OLD.agencyid OR
+                                                                   refer_bibliographicrecordid=OLD.bibliographicrecordid AND refer_agencyid=OLD.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO OLD ID (ID CHANGED)';
+        END IF;
+    END IF;
+    IF (TG_OP = 'UPDATE' AND NEW.deleted) THEN
+        SELECT COUNT(*) > 0 INTO hasRelations FROM relations WHERE bibliographicrecordid=NEW.bibliographicrecordid AND agencyid=NEW.agencyid OR
+                                                                   refer_bibliographicrecordid=NEW.bibliographicrecordid AND refer_agencyid=NEW.agencyid;
+        IF hasRelations THEN
+            RAISE EXCEPTION 'RELATION TO DELETED';
+        END IF;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER validate_relation_fk_rev_update -- V3
+    AFTER UPDATE ON records
+    FOR EACH ROW
+    WHEN (OLD.* IS DISTINCT FROM NEW.*)
+    EXECUTE PROCEDURE validate_relation_fk_rev();
+
+CREATE TRIGGER validate_relation_fk_rev_delete -- V3
+    AFTER DELETE ON records
+    FOR EACH ROW
+    EXECUTE PROCEDURE validate_relation_fk_rev();
+
 
 --
 -- reverse index for getRelationsChildren()
