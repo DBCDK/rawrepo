2eabef9feb4603075042b1b694aee296a8829e34
Morten Bøgeskov <mb@dbc.dk>
2014-10-09 07:28:46 +0000

    Decentral to own queue
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@77537 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/schema/rawrepo-3-content.sql
+++ access/schema/rawrepo-3-content.sql
@@ -1,1 +1,7 @@
+--
+--
+--
+INSERT INTO queueworkers (worker) VALUES('basis-decentral');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('opencataloging-update', 'basis-decentral', 'text/decentral+marcxchange', 'Y', 'A');
+
 UPDATE records SET mimetype='text/enrichment+marcxchange' WHERE EXISTS (SELECT * FROM relations WHERE agencyid=records.agencyid AND bibliographicrecordid=records.bibliographicrecordid AND refer_bibliographicrecordid=records.bibliographicrecordid);
