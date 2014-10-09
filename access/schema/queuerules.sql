c65c2dd9b0592b8950222859e98b6415059ccbba
Morten Bøgeskov <mb@dbc.dk>
2014-10-09 07:28:08 +0000

    Decentral to own queue
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@77536 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/schema/queuerules.sql
+++ access/schema/queuerules.sql
@@ -2,21 +2,27 @@
 -- fbs-sync wants only modified records
 --
 INSERT INTO queueworkers (worker) VALUES('fbs-sync');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'fbs-sync', 'Y', 'A');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('opencataloging-update', 'fbs-sync', '', 'Y', 'A');
 --
 -- broend-sync wants all searchable records affected by modification
 --
 INSERT INTO queueworkers (worker) VALUES('broend-sync');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'broend-sync', 'A', 'Y');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('opencataloging-update', 'broend-sync', '', 'A', 'Y');
 --
 -- solr-sync wants only modified records
 --
 INSERT INTO queueworkers (worker) VALUES('solr-sync');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'solr-sync', 'Y', 'A');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('opencataloging-update', 'solr-sync', '', 'Y', 'A');
+
+--
+--
+--
+INSERT INTO queueworkers (worker) VALUES('basis-decentral');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('opencataloging-update', 'basis-decentral', 'text/decentral+marcxchange', 'Y', 'A');
 
 ---
 --- rules for bulk loader
 ---
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('bulk-fbs', 'fbs-sync', 'Y', 'A');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('bulk-broend', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('bulk-solr', 'solr-sync', 'Y', 'A');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('bulk-fbs', 'fbs-sync', '', 'Y', 'A');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('bulk-broend', 'broend-sync', '', 'A', 'Y');
+INSERT INTO queuerules (provider, worker, mimetype, changed, leaf) VALUES('bulk-solr', 'solr-sync', '', 'Y', 'A');
