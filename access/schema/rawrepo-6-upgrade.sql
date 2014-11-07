5adbfda15e8f1388f7b416ef08c66f9dc15bba8f
Morten Bøgeskov <mb@dbc.dk>
2014-11-07 10:47:05 +0000

    Intermediate version number marking (double dequeue)
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@78790 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ access/schema/rawrepo-6-upgrade.sql
@@ -0,0 +1,33 @@
+--
+-- ENSURE ONLY UPGRADING PREVIOUS VERSION
+--
+\set ON_ERROR_STOP
+
+BEGIN TRANSACTION;
+
+DO
+$$
+DECLARE
+  currentversion INTEGER = 6;
+  brokenversion INTEGER = 0;
+  oldversion INTEGER;
+BEGIN
+  SELECT MAX(version) INTO oldversion FROM version;
+  IF (oldversion <> (currentversion-1)) THEN
+    RAISE EXCEPTION 'Expected schema version % found %', (currentversion-1), oldversion;
+  END IF;
+  INSERT INTO version VALUES(currentversion);
+  DELETE FROM version WHERE version <= brokenversion;
+END
+$$;
+
+
+--
+--
+--
+
+
+--
+--
+--
+COMMIT;
