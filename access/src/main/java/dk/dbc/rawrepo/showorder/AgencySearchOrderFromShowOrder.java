6fe785629ea2cbf71fd48ae5d4dc6f9eca2f8fa1
Morten Bøgeskov <mb@dbc.dk>
2014-12-15 09:12:13 +0000

    Self contained wsdl
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@79967 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/src/main/java/dk/dbc/rawrepo/showorder/AgencySearchOrderFromShowOrder.java
+++ access/src/main/java/dk/dbc/rawrepo/showorder/AgencySearchOrderFromShowOrder.java
@@ -54,8 +54,8 @@ public class AgencySearchOrderFromShowOrder extends AgencySearchOrder {
     public AgencySearchOrderFromShowOrder(String url) throws MalformedURLException {
         new URL(url); // validate url syntax
         URL resource = getClass().getResource("/openagency.wsdl");
-        Service create = OpenAgencyService.create(resource, new QName("OpenAgencyService"));
-        this.service = create.getPort(OpenAgencyService.class);
+        System.out.println("resource = " + resource);
+        this.service = new OpenAgencyService(resource);
         this.service.addPort(null, SOAPBinding.SOAP12HTTP_BINDING, url);
     }
 
