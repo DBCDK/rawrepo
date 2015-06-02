254db2f7dff66812ba827636b4e4e2aaabe8140d
Morten Bøgeskov <mb@dbc.dk>
2015-06-02 09:07:00 +0000

    posthus tag
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@85958 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ marcxchange-bulkload/javascript/posthus/records.js
@@ -0,0 +1,80 @@
+use("Print");
+use("Log");
+use("XmlUtil");
+use("XPath");
+use("XmlNamespaces");
+use("Binary");
+use("PostgreSQL");
+
+var marcx = new Namespace("marcx", "info:lc/xmlns/marcxchange-v1");
+var db = PostgreSQL(System.arguments[0]);
+var parent_agencyid = System.arguments.length > 1 ? System.arguments[1] : "191919";
+
+function begin() {
+}
+
+function end() {
+    print("\n");
+}
+
+function work(r) {
+    Log.trace(r);
+    var xml = XmlUtil.fromString(r);
+
+    var bibliographicrecordid = XPath.selectText('/marcx:record/marcx:datafield[@tag="001"]/marcx:subfield[@code="a"]', xml);
+    var agencyid = XPath.selectText('/marcx:record/marcx:datafield[@tag="001"]/marcx:subfield[@code="b"]', xml);
+    var date = XPath.selectText('/marcx:record/marcx:datafield[@tag="n55"]/marcx:subfield[@code="a"]', xml);
+    XPath.delete('/marcx:record/marcx:datafield[@tag="n55"]', xml);
+
+    Log.debug("bibliographicrecordid=" + bibliographicrecordid);
+    Log.debug("agencyid=" + agencyid);
+    Log.debug("date=" + date);
+
+    if(!date) {
+        var today = new Date();
+        date = ("0" + today.getDate()).substr(-2) + ("0" + (1 + today.getMonth())).substr(-2) + today.getFullYear();
+    }
+
+    var y = date.substr(0, 4);
+    var m = date.substr(4, 2);
+    var d = date.substr(6, 2);
+
+    var blob = '<?xml version="1.0" encoding="UTF-8"?>' + "\n" + xml.toString();
+
+    db.begin();
+
+    var sibling = false;
+    if (agencyid !== parent_agencyid) {
+	var q = db.prepare("SELECT COUNT(*) AS count FROM records WHERE bibliographicrecordid = :bibliographicrecordid AND agencyid = :agencyid");
+	q['bibliographicrecordid'] = bibliographicrecordid;
+	q['agencyid'] = parent_agencyid;
+	q.execute();
+	var r = q.fetch(); 
+	var c = r['count'];
+	if(c === 1)
+	    sibling = true;
+    }
+
+
+    var q = db.prepare("UPDATE records SET CONTENT=encode(:blob, 'BASE64'), mimetype=:mimetype, deleted=FALSE, created=:created, modified=TIMEOFDAY()::TIMESTAMP WHERE bibliographicrecordid=:id AND agencyid=:agencyid");
+    q['blob'] = blob;
+    q['mimetype'] = sibling ? "text/enrichment+marcxchange" : "text/marcxchange";
+    q['created'] = y + "-" + m + "-" + d;
+    q['bibliographicrecordid'] = bibliographicrecordid;
+    q['agencyid'] = agencyid;
+    if (q.execute() === 0) {
+        q.done();
+        q = db.prepare("INSERT INTO records(bibliographicrecordid, agencyid, content, mimetype, deleted, created, modified) VALUES(:id, :agencyid, encode(:blob, 'BASE64'), :mimetype, FALSE, :created, TIMEOFDAY()::TIMESTAMP)");
+        q['bibliographicrecordid'] = bibliographicrecordid;
+        q['agencyid'] = agencyid;
+        q['blob'] = blob;
+	q['mimetype'] = sibling ? "text/enrichment+marcxchange" : "text/marcxchange";
+        q['created'] = y + "-" + m + "-" + d;
+        q.execute();
+    }
+    q.done();
+
+    db.commit();
+    print(sibling ? '+' : '·');
+}
+
---
+++ marcxchange-bulkload/javascript/posthus/relations.js
@@ -0,0 +1,111 @@
+use("Print");
+use("Log");
+use("XmlUtil");
+use("XPath");
+use("XmlNamespaces");
+use("Binary");
+use("PostgreSQL");
+use("Log");
+
+
+var marcx = new Namespace( "marcx", "info:lc/xmlns/marcxchange-v1" );
+var db = PostgreSQL(System.arguments[0]);
+var parent_agencyid = System.arguments.length > 1 ? System.arguments[1] : "191919";
+
+
+function begin() {
+}
+
+function end() {
+    print("\n");
+}
+
+function work(r) {
+    Log.trace(r);
+    var xml = XmlUtil.fromString(r);
+
+    var bibliographicrecordid = XPath.selectText('/marcx:record/marcx:datafield[@tag="001"]/marcx:subfield[@code="a"]', xml);
+    var agencyid = XPath.selectText('/marcx:record/marcx:datafield[@tag="001"]/marcx:subfield[@code="b"]', xml);
+    var parent = XPath.selectText('/marcx:record/marcx:datafield[@tag="014"]/marcx:subfield[@code="a"]', xml);
+
+    Log.debug("bibliographicrecordid=" + bibliographicrecordid);
+    Log.debug("agencyid=" + agencyid);
+    Log.debug("parent=" + parent);
+
+    db.begin();
+    try {
+	try {
+	    var q = db.prepare("DELETE FROM relations WHERE bibliographicrecordid = :bibliographicrecordid AND agencyid = :agencyid");
+	    q['bibliographicrecordid'] = bibliographicrecordid;
+	    q['agencyid'] = agencyid;
+	    q.execute();
+	    q.done();
+        } catch (e) {
+            Log.error("Delete relations for: " + bibliographicrecordid);
+            Log.error(e);
+	    throw e;
+	}
+
+	var s = "·"
+	var sibling = false;
+	if (agencyid !== parent_agencyid) {
+	    Log.debug("Might have sibling");
+	    try {
+		var q = db.prepare("SELECT COUNT(*) AS count FROM records WHERE bibliographicrecordid = :bibliographicrecordid AND agencyid = :agencyid");
+		q['bibliographicrecordid'] = bibliographicrecordid;
+		q['agencyid'] = parent_agencyid;
+		q.execute();
+		var r = q.fetch(); 
+		var c = r['count'];
+		if(c === 1) {
+		    Log.debug("Has sibling");
+		    sibling = true;
+		}
+            } catch (e) {
+		Log.error("Find sibling for: " + bibliographicrecordid);
+		Log.error(e);
+		throw e;
+	    }
+	}
+	
+	if (sibling) {
+	    try {
+		q = db.prepare("INSERT INTO relations(bibliographicrecordid, agencyid, refer_bibliographicrecordid, refer_agencyid) VALUES(:bibliographicrecordid, :agencyid, :refer_bibliographicrecordid, :ref_agencyid)");
+		q['bibliographicrecordid'] = bibliographicrecordid;
+		q['agencyid'] = agencyid;
+		q['refer_bibliographicrecordid'] = bibliographicrecordid;
+		q['refer_agencyid'] = parent_agencyid;
+		q.execute();
+		q.done();
+		s = '«';
+	    } catch (e) {
+		Log.error("ERROR: Sibling for " + bibliographicrecordid + " from " + agencyid + " to " + parent_agencyid);
+		Log.warn(e);
+		throw e;
+	    }
+	} else if (parent !== "") {
+	    try {
+		q = db.prepare("INSERT INTO relations(bibliographicrecordid, agencyid, refer_bibliographicrecordid, refer_agencyid) VALUES(:id, :agencyid, :ref_id, :ref_agencyid)");
+		q['bibliographicrecordid'] = bibliographicrecordid;
+		q['agencyid'] = agencyid;
+		q['refer_bibliographicrecordid'] = parent;
+		q['refer_agencyid'] = agencyid;
+		q.execute();
+		q.done();
+		s = "^";
+	    } catch (e) {
+		Log.error("ERROR: Parent " + parent + " for " + bibliographicrecordid + " from " + agencyid);
+		Log.warn(e);
+		throw e;
+            }
+	}
+	print(s);
+	
+	db.commit();
+    } catch (e) {
+	print("*");
+        db.rollback();
+	db.begin();
+        throw e;
+    }
+}
