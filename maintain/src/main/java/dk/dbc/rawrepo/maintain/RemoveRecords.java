c75b352783609a9ae4265d1385d5ae8d00f76214
Morten Bøgeskov <mb@dbc.dk>
2016-12-23 06:38:25 +0000

    Create full delete documents
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@108047 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- maintain/src/main/java/dk/dbc/rawrepo/maintain/RemoveRecords.java
+++ maintain/src/main/java/dk/dbc/rawrepo/maintain/RemoveRecords.java
@@ -21,16 +21,20 @@
 package dk.dbc.rawrepo.maintain;
 
 import dk.dbc.marcxmerge.MarcXChangeMimeType;
+import dk.dbc.marcxmerge.MarcXMerger;
+import dk.dbc.marcxmerge.MarcXMergerException;
 import dk.dbc.openagency.client.OpenAgencyServiceFromURL;
 import dk.dbc.rawrepo.RawRepoDAO;
 import dk.dbc.rawrepo.RawRepoException;
 import dk.dbc.rawrepo.Record;
-import dk.dbc.rawrepo.RecordId;
 import dk.dbc.rawrepo.maintain.transport.StandardResponse;
 import dk.dbc.rawrepo.maintain.transport.StandardResponse.Result.Status;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -44,8 +48,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.sql.DataSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -55,10 +75,15 @@ public class RemoveRecords extends RawRepoWorker {
 
     private static final Logger log = LoggerFactory.getLogger(RemoveRecords.class);
 
-    private static final DataTemplate template = new DataTemplate("content.xml");
+    private final DocumentBuilder documentBuilder;
+    private final Transformer transformer;
+    private final MarcXMerger marcXMerger;
 
-    public RemoveRecords(DataSource dataSource, OpenAgencyServiceFromURL openAgency, ExecutorService executorService) {
+    public RemoveRecords(DataSource dataSource, OpenAgencyServiceFromURL openAgency, ExecutorService executorService) throws MarcXMergerException {
         super(dataSource, openAgency, executorService);
+        this.documentBuilder = newDocumentBuilder();
+        this.transformer = newTransformer();
+        this.marcXMerger = new MarcXMerger();
     }
 
     public HashMap<String, ArrayList<String>> getValues(HashMap<String, List<String>> valuesSet, String leaving) {
@@ -90,7 +115,8 @@ public class RemoveRecords extends RawRepoWorker {
                     removeRecord(agencyId, id, provider, trackingId);
                     connection.commit();
                     success++;
-                } catch (RawRepoException ex) {
+                } catch (RawRepoException | DOMException | IOException | MarcXMergerException |
+                        SAXException | TransformerException ex) {
                     failed++;
                     diags.add(new StandardResponse.Result.Diag("Record: " + id, ex.getMessage()));
                     Throwable cause = ex.getCause();
@@ -120,15 +146,13 @@ public class RemoveRecords extends RawRepoWorker {
             log.error("Error getting database connection: " + ex.getMessage());
             return new StandardResponse.Result(StandardResponse.Result.Status.FAILURE, "Error getting database connection");
         }
-
     }
 
-    void removeRecord(Integer agencyId, String id, String provider, String trackingId) throws SQLException, RawRepoException {
+    void removeRecord(Integer agencyId, String id, String provider, String trackingId) throws SQLException, RawRepoException, MarcXMergerException, SAXException, TransformerException, DOMException, IOException {
         RawRepoDAO dao = getDao();
-        Record record = dao.fetchRecord(id, agencyId);
-        if (record.isOriginal()) {
-            throw new RawRepoException("Record does not exist");
-        }
+
+        Record record = dao.fetchMergedRecord(id, agencyId, marcXMerger, true);
+
         if (record.isDeleted()) {
             throw new RawRepoException("Record already deleted");
         }
@@ -138,85 +162,145 @@ public class RemoveRecords extends RawRepoWorker {
         if (!dao.getRelationsSiblingsToMe(record.getId()).isEmpty()) {
             throw new RawRepoException("There's relations to this record (has siblings)");
         }
+
         dao.changedRecord(provider, record.getId());
-        dao.setRelationsFrom(record.getId(), new HashSet<>());
-        record.setDeleted(true);
+
+        if (record.getId().getAgencyId() != agencyId) {
+            log.debug("Creating record for: " + id);
+            Record r = dao.fetchRecord(id, agencyId);
+            r.setContent(record.getContent());
+            r.setMimeType(record.getMimeType());
+            record = r;
+        }
+        byte[] content = markMarcContentDeleted(record.getContent());
+        record.setContent(content);
         if (record.getMimeType().equals(MarcXChangeMimeType.ENRICHMENT)) {
             record.setMimeType(MarcXChangeMimeType.MARCXCHANGE);
         }
-        String content = template.build("agencyid", String.format("%06d", agencyId),
-                                        "bibliographicrecordid", id);
-        record.setContent(content.getBytes(StandardCharsets.UTF_8));
+        record.setDeleted(true);
         if (trackingId != null) {
             record.setTrackingId(trackingId);
         }
+        dao.deleteRelationsFrom(record.getId());
         dao.saveRecord(record);
+
     }
 
+    byte[] markMarcContentDeleted(byte[] content) throws SAXException, TransformerException, DOMException, IOException {
+        Document dom = documentBuilder.parse(new ByteArrayInputStream(content));
+        Element marcx = dom.getDocumentElement();
+        Node child = marcx.getFirstChild();
+        for (;;) {
+            if (child == null ||
+                ( child.getNodeType() == Node.ELEMENT_NODE &&
+                  "datafield".equals(child.getLocalName()) )) {
+                int cmp = -1;
+                if (child != null) {
+                    String tag = ( (Element) child ).getAttribute("tag");
+                    cmp = "004".compareTo(tag);
+                }
+                if (child == null || cmp < 0) {
+                    Element n = dom.createElementNS("info:lc/xmlns/marcxchange-v1", "datafield");
+                    n.setAttribute("tag", "004");
+                    n.setAttribute("ind1", "0");
+                    n.setAttribute("ind2", "0");
+                    marcx.insertBefore(n, child);
+                    child = n;
+                }
+                if (cmp <= 0) {
+                    Node subChild = child.getFirstChild();
+                    for (;;) {
+                        // http://www.kat-format.dk/danMARC2/Danmarc2.7.htm
+                        // r is 1st field
+                        if (subChild == null || ( subChild.getNodeType() == Node.ELEMENT_NODE &&
+                                                  "subfield".equals(subChild.getLocalName()) )) {
+                            boolean isR = false;
+                            if (subChild != null) {
+                                String code = ( (Element) subChild ).getAttribute("code");
+                                isR = "r".equals(code);
+                            }
+                            if (!isR) {
+                                Element n = dom.createElementNS("info:lc/xmlns/marcxchange-v1", "subfield");
+                                n.setAttribute("code", "r");
+                                child.insertBefore(n, subChild);
+                                subChild = n;
+                            }
+                            while (subChild.hasChildNodes()) {
+                                subChild.removeChild(subChild.getFirstChild());
+                            }
+                            subChild.appendChild(dom.createTextNode("d"));
+                            subChild = subChild.getNextSibling();
+                            while (subChild != null) {
+                                Node next = subChild.getNextSibling();
+                                if (subChild.getNodeType() == Node.ELEMENT_NODE &&
+                                    "subfield".equals(subChild.getLocalName()) &&
+                                    "r".equals(( (Element) subChild ).getAttribute("code"))) {
+                                    child.removeChild(subChild);
+                                }
+                                subChild = next;
+                            }
+                            break;
 
-    /*
-     *     ______                     __      __
-     *    /_  __/__  ____ ___  ____  / /___ _/ /____
-     *     / / / _ \/ __ `__ \/ __ \/ / __ `/ __/ _ \
-     *    / / /  __/ / / / / / /_/ / / /_/ / /_/  __/
-     *   /_/  \___/_/ /_/ /_/ .___/_/\__,_/\__/\___/
-     *                     /_/
+                        }
+                        subChild = subChild.getNextSibling();
+                    }
+                    break;
+                }
+            }
+            child = child.getNextSibling();
+        }
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        transformer.transform(
+                new DOMSource(dom),
+                new StreamResult(new OutputStreamWriter(os, StandardCharsets.UTF_8)));
+        return os.toByteArray();
+    }
+
+    /**
+     * Create an xml document parser
+     *
+     * @return
+     * @throws ParserConfigurationException
      */
-    private static class DataTemplate {
-
-        private static final Pattern VARIABLE = Pattern.compile("\\$\\{([0-9a-z](?:[-.]?[0-9a-z])*)\\}");
-        private ArrayList<String> list; // odd number entries are verbatim, even are properties
-
-        public DataTemplate(String file) {
-
-            try {
-                InputStream stream = getClass().getClassLoader().getResourceAsStream(file);
-                if (stream == null) {
-                    throw new FileNotFoundException(file);
-                }
-                byte[] bytes = new byte[stream.available()];
-                int read = stream.read(bytes);
-                if (read != bytes.length) {
-                    throw new IOException("Shortread expected " + bytes.length + " got " + read);
-                }
-                this.list = new ArrayList<>();
-                String data = new String(bytes, "UTF-8");
-                Matcher matcher = VARIABLE.matcher(data);
-                int start = 0;
-                while (matcher.find()) {
-                    list.add(data.substring(start, matcher.start()));
-                    list.add(matcher.group(1));
-                    start = matcher.end();
-                }
-                list.add(data.substring(start));
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
+    private static DocumentBuilder newDocumentBuilder() throws MarcXMergerException {
+        try {
+            synchronized (DocumentBuilderFactory.class) {
+                DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+                documentBuilderFactory.setNamespaceAware(true);
+                documentBuilderFactory.setIgnoringComments(true);
+                documentBuilderFactory.setIgnoringElementContentWhitespace(true);
+                return documentBuilderFactory.newDocumentBuilder();
             }
-        }
-
-        public String build(Properties props) {
-            return build(props, "[UNSET]");
-        }
-
-        public String build(Properties props, String defaultValue) {
-            StringBuilder sb = new StringBuilder();
-            for (Iterator<String> iterator = list.iterator() ; iterator.hasNext() ;) {
-                sb.append(iterator.next());
-                if (iterator.hasNext()) {
-                    sb.append(props.getProperty(iterator.next(), defaultValue));
-                }
-            }
-            return sb.toString();
-        }
-
-        public String build(String... s) {
-            Properties props = new Properties();
-            int i = 0;
-            while (i < s.length - 1) {
-                props.put(s[i], s[i + 1]);
-                i = i + 2;
-            }
-            return build(props, i < s.length ? s[i] : "[UNSET]");
+        } catch (ParserConfigurationException ex) {
+            log.error("Cannot create parser part of xml merger", ex);
+            throw new MarcXMergerException("Cannot init record merger", ex);
         }
     }
+
+    /**
+     * Create an xml transformer for writing a document
+     *
+     * @return new transformer
+     * @throws TransformerConfigurationException
+     * @throws TransformerFactoryConfigurationError
+     * @throws IllegalArgumentException
+     */
+    private static Transformer newTransformer() throws MarcXMergerException {
+        try {
+            synchronized (TransformerFactory.class) {
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+                transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+                transformer.setOutputProperty(OutputKeys.INDENT, "no");
+                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+                return transformer;
+            }
+        } catch (TransformerFactoryConfigurationError | TransformerConfigurationException | IllegalArgumentException ex) {
+            log.error("Cannot create writer part of xml merger", ex);
+            throw new MarcXMergerException("Cannot init record merger", ex);
+        }
+    }
+
 }
