b7821a3e99bca442bf10c8c40e36e3d1041bd7ea
Anders Thrane Michelsen <atm@dbc.dk>
2017-10-13 11:04:19 +0000

    Updated queuerules to match what is actually in production
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@116424 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/schema/queuerules.sql
+++ access/schema/queuerules.sql
@@ -12,59 +12,74 @@
 --
 -- dbc-rawrepo-access is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with dbc-rawrepo-access.  If not, see <http://www.gnu.org/licenses/>.
+-- along with dbc-rawrepo-access. If not, see <http://www.gnu.org/licenses/>.
 -- 
 
 --
 -- broend-sync wants all searchable records affected by modification
 --
-INSERT INTO queueworkers (worker) VALUES('broend-sync');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'broend-sync', 'A', 'Y');
---
--- solr-sync wants only modified records
---
-INSERT INTO queueworkers (worker) VALUES('solr-sync');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'solr-sync', 'Y', 'A');
+INSERT INTO queueworkers (worker) VALUES ('basis-decentral');
+INSERT INTO queueworkers (worker) VALUES ('broend-sync');
+INSERT INTO queueworkers (worker) VALUES ('broend30-ph-sync');
+INSERT INTO queueworkers (worker) VALUES ('broend30-sync');
+INSERT INTO queueworkers (worker) VALUES ('danbib-libv3');
+INSERT INTO queueworkers (worker) VALUES ('danbib-ph-libv3');
+INSERT INTO queueworkers (worker) VALUES ('dataio-bulk-sync');
+INSERT INTO queueworkers (worker) VALUES ('dataio-socl-sync-bulk');
+INSERT INTO queueworkers (worker) VALUES ('ims-bulk-sync');
+INSERT INTO queueworkers (worker) VALUES ('ims-sync');
+INSERT INTO queueworkers (worker) VALUES ('oai-set-matcher');
+INSERT INTO queueworkers (worker) VALUES ('socl-sync');
+INSERT INTO queueworkers (worker) VALUES ('socl-sync-prio1');
+INSERT INTO queueworkers (worker) VALUES ('solr-a-sync');
+INSERT INTO queueworkers (worker) VALUES ('solr-b-sync');
+INSERT INTO queueworkers (worker) VALUES ('solr-sync-bulk');
 
---
---
---
-INSERT INTO queueworkers (worker) VALUES('basis-decentral');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('opencataloging-update', 'basis-decentral', 'Y', 'A');
 
-INSERT INTO queueworkers (worker) VALUES('solr-sync-bulk');
-
----
---- rules for bulk loader
----
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('bulk-broend', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('bulk-solr', 'solr-sync-bulk', 'Y', 'A');
-
----
---- rules for agency-delete
----
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('agency-delete', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('agency-delete', 'solr-sync', 'Y', 'A');
-
----
---- rules for agency-maintain
----
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('agency-maintain', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('agency-maintain', 'solr-sync', 'Y', 'A');
-
----
---- rules for fbs-update
----
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('fbs-update', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('fbs-update', 'solr-sync', 'Y', 'A');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('fbs-update', 'basis-decentral', 'Y', 'A');
-
----
---- rules for dataio-update
----
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('dataio-update', 'broend-sync', 'A', 'Y');
-INSERT INTO queuerules (provider, worker, changed, leaf) VALUES('dataio-update', 'solr-sync', 'Y', 'A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-delete','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-delete','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-maintain','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-maintain','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency30-delete','broend30-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency3x-delete','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency3x-delete','broend30-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency3x-delete','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('bulk-broend','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-bulk','dataio-bulk-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-bulk','dataio-socl-sync-bulk','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-bulk','oai-set-matcher','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-ph-holding-update','broend30-ph-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-ph-holding-update','danbib-ph-libv3','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update','oai-set-matcher','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-all_display_bases','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-all_display_bases','broend30-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-all_display_bases','danbib-libv3','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-all_display_bases','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-danbib','danbib-libv3','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-danbib','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.0','broend30-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.5','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.5','ims-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.5','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.x','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.x','broend30-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('dataio-update-well3.x','socl-sync','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-ph-update','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-ph-update','broend30-ph-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-ph-update','danbib-ph-libv3','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-ph-update','socl-sync-prio1','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-update','basis-decentral','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-update','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-update','ims-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('fbs-update','socl-sync-prio1','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('ims','ims-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('ims-bulk','ims-bulk-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('opencataloging-update','basis-decentral','Y','A');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('opencataloging-update','broend-sync','A','Y');
+INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('opencataloging-update','socl-sync','Y','A');
