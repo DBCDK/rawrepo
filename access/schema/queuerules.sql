0cbed1d5b0c5940e2b94ddb7ee91682e6c4b68e9
Anders Thrane Michelsen <atm@dbc.dk>
2017-11-24 06:48:58 +0000

    MS-1451 - Use correct short license header
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@117229 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/schema/queuerules.sql
+++ access/schema/queuerules.sql
@@ -1,26 +1,6 @@
--- 
--- dbc-rawrepo-access
--- Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
--- Denmark. CVR: 15149043
 --
--- This file is part of dbc-rawrepo-access.
---
--- dbc-rawrepo-access is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
---
--- dbc-rawrepo-access is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with dbc-rawrepo-access. If not, see <http://www.gnu.org/licenses/>.
--- 
-
---
--- broend-sync wants all searchable records affected by modification
+-- Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+--  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
 --
 INSERT INTO queueworkers (worker) VALUES ('basis-decentral');
 INSERT INTO queueworkers (worker) VALUES ('broend-sync');
@@ -38,7 +18,6 @@ INSERT INTO queueworkers (worker) VALUES ('socl-sync-prio1');
 INSERT INTO queueworkers (worker) VALUES ('solr-a-sync');
 INSERT INTO queueworkers (worker) VALUES ('solr-b-sync');
 INSERT INTO queueworkers (worker) VALUES ('solr-sync-bulk');
-
 
 INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-delete','broend-sync','A','Y');
 INSERT INTO queuerules (provider, worker, changed, leaf) VALUES ('agency-delete','socl-sync','Y','A');
