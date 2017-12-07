0fbe89caadad8e788b1dfbfbf90f96310dd818e2
Anders Thrane Michelsen <atm@dbc.dk>
2017-12-07 13:01:04 +0000

    BUG-21477 Implement review comments - "Uncollapsed" import - Removed dev debugging comments - Replaced copy right header with short version
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@117648 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- access/src/main/java/dk/dbc/rawrepo/RelationHintsOpenAgency.java
+++ access/src/main/java/dk/dbc/rawrepo/RelationHintsOpenAgency.java
@@ -1,23 +1,8 @@
 /*
- * dbc-rawrepo-access
- * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
- * Denmark. CVR: 15149043
- *
- * This file is part of dbc-rawrepo-access.
- *
- * dbc-rawrepo-access is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * dbc-rawrepo-access is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with dbc-rawrepo-access.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
  */
+
 package dk.dbc.rawrepo;
 
 import dk.dbc.openagency.client.LibraryRuleHandler;
@@ -26,15 +11,15 @@ import dk.dbc.openagency.client.OpenAgencyServiceFromURL;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 /**
- *
  * @author DBC {@literal <dbc.dk>}
  */
 public class RelationHintsOpenAgency extends RelationHints {
-    private static final XLogger logger = XLoggerFactory.getXLogger(RelationHintsOpenAgency.class);
     private final OpenAgencyServiceFromURL openAgencyService;
 
     public RelationHintsOpenAgency(OpenAgencyServiceFromURL openAgencyService) {
@@ -69,22 +54,20 @@ public class RelationHintsOpenAgency extends RelationHints {
     }
 
     @Override
-    public List<Integer> getProviderOptions(int agencyId) throws RawRepoException{
+    public List<Integer> getProviderOptions(int agencyId) throws RawRepoException {
         List<Integer> agencyPriorityList = new ArrayList<>();
         agencyPriorityList.add(agencyId); // Always use own agency first
 
         if (usesCommonSchoolAgency(agencyId)) {
-            logger.info("School");
             agencyPriorityList.add(300000); // School libraries must use agency 300000 if the local record doesn't exist
             agencyPriorityList.add(870970);
             agencyPriorityList.add(870979);
         } else if (usesCommonAgency(agencyId)) {
-            logger.info("FBS");
             agencyPriorityList.add(870970);
             agencyPriorityList.add(870979);
         }
 
         return agencyPriorityList;
-     }
+    }
 
 }
