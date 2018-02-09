f52cea20efdd812379392b2a7de6dd0222d06206
Anders Thrane Michelsen <atm@dbc.dk>
2018-02-09 11:08:49 +0000

    Remove queued timestamp from from bulk insert
    If queued was included in values then all rows would get the exact same
    timestamp. This way the queued timestamp will differ a little bit and
    therefor produce more real-life like data.
    
    git-svn-id: https://svn.dbc.dk/repos/rawrepo/trunk@118763 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- performance-test/populate_db.py
+++ performance-test/populate_db.py
@@ -4,8 +4,8 @@ size = 1000000
 worker_name = 'performance_test'
 
 print("insert into queueworkers values('performance_test');")
-print("COPY queue (bibliographicrecordid, agencyid, worker, queued) FROM stdin;")
+print("COPY queue (bibliographicrecordid, agencyid, worker) FROM STDIN;")
 for _ in range(size):
-    print("{}\t{}\t{}\t{}".format(random.randint(100000000, 999999999), '123456', worker_name, 'now()'))
+    print("{}\t{}\t{}".format(random.randint(100000000, 999999999), '123456', worker_name))
 print('\\.')
 print(' ')
