d3e472e2bd6a264d7b3d2038504a21db11350a1e
Anders Thrane Michelsen <atm@dbc.dk>
2018-03-21 08:09:22 +0100

    Fix integration test
    A new column was recently added to queuerules which broke the maintain
    integration tests. This has been fixed by adding dummy values to the new
    column.

--- maintain/src/build-test-resources/data/300-queue.sql
+++ maintain/src/build-test-resources/data/300-queue.sql
@@ -1,5 +1,5 @@
 INSERT INTO queueworkers VALUES('changed');
 INSERT INTO queueworkers VALUES('leaf');
 
-INSERT INTO queuerules VALUES('test', 'changed', '', 'Y', 'A');
-INSERT INTO queuerules VALUES('test', 'leaf', '', 'A', 'Y');
+INSERT INTO queuerules VALUES('test', 'changed', '', 'Y', 'A', null);
+INSERT INTO queuerules VALUES('test', 'leaf', '', 'A', 'Y', null);
