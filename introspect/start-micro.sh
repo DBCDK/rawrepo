5f2aa004416ffe7758eda922b4f3f0718ccbb4c7
Anders Thrane Michelsen <atm@dbc.dk>
2019-02-19 09:58:22 +0100

    Move Dockerfiles for access and introspect to the projects

--- introspect/start-micro.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-java $JAVA_OPTS \
-	 	-Djava.util.logging.config.file=./logging.properties \
-	 	-Dcontainer.hostname=$HOSTNAME \
-	 	-cp "/home/ja7/ftp/payara-micro-4.1.2.173.jar:./json-log-formater.jar" json-logging fish.payara.micro.PayaraMicro --deploy target/rawrepo-introspect-1.5-SNAPSHOT.war --port 8081 \
-		--logproperties ./logging.properties
