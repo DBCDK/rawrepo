2881bd22d4b1e362cdb24dd2843de7bbaf001ccb
atmdbc <atm@dbc.dk>
2020-01-22 12:43:22 +0100

    Update openagency url

--- access/schema/configurations.sql
+++ access/schema/configurations.sql
@@ -1,2 +1,2 @@
 INSERT INTO configurations (key, value) VALUES ('RECORD_SERVICE_URL', 'http://rawrepo-record-service');
-INSERT INTO configurations (key, value) VALUES ('OPENAGENCY_URL', 'http://openagency.addi.dk/test_2.34/');
+INSERT INTO configurations (key, value) VALUES ('OPENAGENCY_URL', 'http://vip-openagency/test_2.34/');
