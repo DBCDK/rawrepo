21fa6f6be31567443c0a155dee88d8e0aa388f1e
atmdbc <atm@dbc.dk>
2020-03-27 13:49:50 +0100

    Replace triggers with functions
    Auditors: jbn, jbr

---
+++ access/schema/rawrepo-28-upgrade.sql
@@ -0,0 +1,86 @@
+--
+-- ENSURE ONLY UPGRADING PREVIOUS VERSION
+--
+\set ON_ERROR_STOP
+
+BEGIN TRANSACTION;
+
+DO
+$$
+DECLARE
+  currentversion INTEGER = 28;
+  brokenversion  INTEGER = 20;
+  OLDversion     INTEGER;
+BEGIN
+  SELECT MAX(version)
+  INTO OLDversion
+  FROM version;
+  IF (OLDversion <> (currentversion - 1))
+  THEN
+    RAISE EXCEPTION 'Expected schema version % found %', (currentversion - 1), OLDversion;
+  END IF;
+  INSERT INTO version VALUES (currentversion);
+  DELETE FROM version
+  WHERE version = brokenversion;
+END
+$$;
+
+--
+--
+--
+
+DROP TRIGGER records_insert_trig_summary ON records;
+DROP TRIGGER records_update_trig_summary ON records;
+DROP TRIGGER records_delete_trig_summary ON records;
+
+DROP FUNCTION insert_records_summary();
+DROP FUNCTION update_records_summary();
+DROP FUNCTION delete_records_summary();
+
+CREATE OR replace FUNCTION refresh_records_summary() returns setof records_summary
+AS
+  $$
+BEGIN
+  TRUNCATE records_summary;
+  INSERT INTO records_summary
+  SELECT   agencyid,
+           count(*) filter (WHERE deleted = 'F' AND mimetype != 'text/enrichment+marcxchange') AS original_count,
+           count(*) filter (WHERE deleted = 'F' AND mimetype = 'text/enrichment+marcxchange') AS enrichment_count,
+           count(*) filter (WHERE deleted = 'T') AS deleted_count,
+           max(modified) AS ajour_date
+  FROM     records
+  GROUP BY agencyid
+  ORDER BY agencyid;
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR replace FUNCTION refresh_records_summary_by_agencyId(agencyid_ NUMERIC(6))
+    RETURNS SETOF records_summary AS $$ -- V28
+DECLARE
+  row    records_summary;
+BEGIN
+    SELECT agencyId,
+           count(*) FILTER (WHERE deleted = 'F' AND mimetype != 'text/enrichment+marcxchange') AS original_count,
+           count(*) FILTER (WHERE deleted = 'F' AND mimetype = 'text/enrichment+marcxchange') AS enrichment_count,
+           count(*) FILTER (WHERE deleted = 'T') AS deleted_count,
+           max(modified) AS ajour_date
+    FROM records
+    INTO row
+    WHERE agencyId = agencyid_
+    GROUP BY agencyid
+    ORDER BY agencyid;
+
+    INSERT INTO records_summary (agencyId, original_count, enrichment_count, deleted_count, ajour_date) VALUES (agencyid_, row.original_count , row.enrichment_count, row.deleted_count, row.ajour_date)
+    ON CONFLICT (agencyid)
+        DO UPDATE SET original_count = row.original_count,
+            enrichment_count = row.enrichment_count,
+            deleted_count = row.deleted_count,
+            ajour_date = row.ajour_date;
+
+  RETURN;
+END;
+$$ LANGUAGE plpgsql;
+
+COMMIT TRANSACTION;
